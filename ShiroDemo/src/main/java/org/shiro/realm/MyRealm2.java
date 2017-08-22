df5818272f5c789b66adcc45a4e3f59f72677db6
Administrator <644724460@qq.com>
2017-08-23 07:43:07 +0800

    Authenticator及AuthenticationStrategy

---
+++ ShiroDemo/src/main/java/org/shiro/realm/MyRealm2.java
@@ -0,0 +1,31 @@
+package org.shiro.realm;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * Created by Administrator on 2017/8/22.
+ */
+public class MyRealm2 implements Realm{
+    public String getName() {
+        return "MyRealm2";
+    }
+
+    public boolean supports(AuthenticationToken authenticationToken) {
+        //仅支持UsernamePasswordToken类型的Token
+        return authenticationToken instanceof UsernamePasswordToken;
+    }
+
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        String username= (String) authenticationToken.getPrincipal();
+        String password= (String) authenticationToken.getCredentials();
+        if(!"wang".equals(username)){
+            throw new UnknownAccountException();//如果用户名错误
+        }
+        if(!"123".equals(password)){
+            throw new  IncorrectCredentialsException();//如果密码错误
+        }
+        //如果身份认证验证成功，返回一个AuthenticationInfo实现；
+        return new SimpleAuthenticationInfo(username,password,getName());
+    }
+}
--- ShiroDemo/src/test/java/demo2/AuthenticatorTest.java
+++ ShiroDemo/src/test/java/demo2/AuthenticatorTest.java
@@ -1,7 +1,31 @@
 package demo2;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+
 /**
  * Created by s on 2017/8/22.
  */
 public class AuthenticatorTest {
+
+    //首先通用化登录逻辑
+    private void login(String configFile) {
+        //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+        Factory<SecurityManager> factory =
+                new IniSecurityManagerFactory(configFile);
+
+        //2、得到SecurityManager实例 并绑定给SecurityUtils
+        org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        //3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+
+        subject.login(token);
+    }
 }
