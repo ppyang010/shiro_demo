a93c19aedffae37b501a854f863aa7a91e6d1665
ppyang010 <644724460@qq.com>
2017-08-22 19:35:53 +0800

    jdbc

--- ShiroDemo/src/main/java/org/shiro/realm/MyRealm1.java
+++ ShiroDemo/src/main/java/org/shiro/realm/MyRealm1.java
@@ -1,9 +1,6 @@
 package org.shiro.realm;
 
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.AuthenticationInfo;
-import org.apache.shiro.authc.AuthenticationToken;
-import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.realm.Realm;
 
 /**
@@ -21,6 +18,14 @@ public class MyRealm1 implements Realm{
 
     public AuthenticationInfo getAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
         String username= (String) authenticationToken.getPrincipal();
-        return null;
+        String password= (String) authenticationToken.getCredentials();
+        if(!"zhang".equals(username)){
+            throw new UnknownAccountException();//如果用户名错误
+        }
+        if(!"123".equals(password)){
+            throw new  IncorrectCredentialsException();//如果密码错误
+        }
+        //如果身份认证验证成功，返回一个AuthenticationInfo实现；
+        return new SimpleAuthenticationInfo(username,password,getName());
     }
 }
---
+++ ShiroDemo/src/test/java/demo2/AuthenticatorTest.java
@@ -0,0 +1,7 @@
+package demo2;
+
+/**
+ * Created by s on 2017/8/22.
+ */
+public class AuthenticatorTest {
+}
